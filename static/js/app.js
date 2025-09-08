11a1a578c3e6f2ddd97c407ae5c66df4fb5269b6
Soffia20 <sofi.alvarez.gzz@gmail.com>
2025-09-07 22:22:04 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -97,7 +97,7 @@ app.controller("productosCtrl", function ($scope, $http) {
     var pusher = new Pusher("e57a8ad0a9dc2e83d9a2", {
       cluster: "us2"
     })
-
+    
     var channel = pusher.subscribe("canalProductos")
     channel.bind("eventoProductos", function(data) {
         // alert(JSON.stringify(data))
@@ -153,6 +153,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     activeMenuOption(location.hash)
 })
+
 
 
 
