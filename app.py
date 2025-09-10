7865d0af625d105efa95f89110c17791d10b04b7
Soffia20 <sofi.alvarez.gzz@gmail.com>
2025-09-09 23:41:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ from flask_cors import CORS, cross_origin
 
 con = mysql.connector.connect(
     host="185.232.14.52",
-    database="u760464709_16005339_bd",
-    user="u760464709_16005339_usr",
-    password="/iJRzrJBz+P1"
+    database="u760464709_23005256_bd",
+    user="u760464709_23005256_usr",
+    password="~6ru!MMJZzX"
 )
 
 app = Flask(__name__)
@@ -267,6 +267,198 @@ def eliminarProducto():
     sql    = """
     DELETE FROM productos
     WHERE Id_Producto = %s
+    """
+    val    = (id,)
+
+    cursor.execute(sql, val)
+    con.commit()
+    con.close()
+
+    return make_response(jsonify({}))
+
+# CLIENTES
+
+@app.route("/clientes")
+def clientes():
+    return render_template("clientes.html")
+
+@app.route("/tbodyClientes")
+def tbodyClientes():
+    if not con.is_connected():
+        con.reconnect()
+
+    cursor = con.cursor(dictionary=True)
+    sql    = """
+    SELECT idCliente,
+           nombreCliente,
+           telefono,
+           correoElectronico
+
+    FROM clientes
+
+    ORDER BY idCliente DESC
+
+    LIMIT 10 OFFSET 0
+    """
+
+    cursor.execute(sql)
+    registros = cursor.fetchall()
+
+    # Si manejas fechas y horas
+    """
+    for registro in registros:
+        fecha_hora = registro["Fecha_Hora"]
+
+        registro["Fecha_Hora"] = fecha_hora.strftime("%Y-%m-%d %H:%M:%S")
+        registro["Fecha"]      = fecha_hora.strftime("%d/%m/%Y")
+        registro["Hora"]       = fecha_hora.strftime("%H:%M:%S")
+    """
+
+    return render_template("tbodyClientes.html", clientes=registros)
+
+# @app.route("/clientes/ingredientes/<int:id>")
+# def productosIngredientes(id):
+#     if not con.is_connected():
+#         con.reconnect()
+
+#     cursor = con.cursor(dictionary=True)
+#     sql    = """
+#     SELECT productos.Nombre_Producto, ingredientes.*, productos_ingredientes.Cantidad FROM productos_ingredientes
+#     INNER JOIN productos ON productos.Id_Producto = productos_ingredientes.Id_Producto
+#     INNER JOIN ingredientes ON ingredientes.Id_Ingrediente = productos_ingredientes.Id_Ingrediente
+#     WHERE productos_ingredientes.Id_Producto = %s
+#     ORDER BY productos.Nombre_Producto
+#     """
+
+#     cursor.execute(sql, (id, ))
+#     registros = cursor.fetchall()
+
+#     return render_template("modal.html", productosIngredientes=registros)
+
+@app.route("/clientes/buscar", methods=["GET"])
+def buscarProductos():
+    if not con.is_connected():
+        con.reconnect()
+
+    args     = request.args
+    busqueda = args["busqueda"]
+    busqueda = f"%{busqueda}%"
+    
+    cursor = con.cursor(dictionary=True)
+    sql    = """
+    SELECT Id_Producto,
+           Nombre_Producto,
+           Precio,
+           Existencias
+
+    FROM productos
+
+    WHERE Nombre_Producto LIKE %s
+    OR    Precio          LIKE %s
+    OR    Existencias     LIKE %s
+
+    ORDER BY Id_Producto DESC
+
+    LIMIT 10 OFFSET 0
+    """
+    val    = (busqueda, busqueda, busqueda)
+
+    try:
+        cursor.execute(sql, val)
+        registros = cursor.fetchall()
+
+        # Si manejas fechas y horas
+        """
+        for registro in registros:
+            fecha_hora = registro["Fecha_Hora"]
+
+            registro["Fecha_Hora"] = fecha_hora.strftime("%Y-%m-%d %H:%M:%S")
+            registro["Fecha"]      = fecha_hora.strftime("%d/%m/%Y")
+            registro["Hora"]       = fecha_hora.strftime("%H:%M:%S")
+        """
+
+    except mysql.connector.errors.ProgrammingError as error:
+        print(f"Ocurrió un error de programación en MySQL: {error}")
+        registros = []
+
+    finally:
+        con.close()
+
+    return make_response(jsonify(registros))
+
+@app.route("/producto", methods=["POST"])
+# Usar cuando solo se quiera usar CORS en rutas específicas
+# @cross_origin()
+def guardarProducto():
+    if not con.is_connected():
+        con.reconnect()
+
+    id          = request.form["id"]
+    nombre      = request.form["nombre"]
+    precio      = request.form["precio"]
+    existencias = request.form["existencias"]
+    # fechahora   = datetime.datetime.now(pytz.timezone("America/Matamoros"))
+    
+    cursor = con.cursor()
+
+    if id:
+        sql = """
+        UPDATE productos
+
+        SET Nombre_Producto = %s,
+            Precio          = %s,
+            Existencias     = %s
+
+        WHERE Id_Producto = %s
+        """
+        val = (nombre, precio, existencias, id)
+    else:
+        sql = """
+        INSERT INTO productos (Nombre_Producto, Precio, Existencias)
+                    VALUES    (%s,          %s,      %s)
+        """
+        val =                 (nombre, precio, existencias)
+    
+    cursor.execute(sql, val)
+    con.commit()
+    con.close()
+
+    pusherProductos()
+    
+    return make_response(jsonify({}))
+
+@app.route("/producto/<int:id>")
+def editarProducto(id):
+    if not con.is_connected():
+        con.reconnect()
+
+    cursor = con.cursor(dictionary=True)
+    sql    = """
+    SELECT Id_Producto, Nombre_Producto, Precio, Existencias
+
+    FROM productos
+
+    WHERE Id_Producto = %s
+    """
+    val    = (id,)
+
+    cursor.execute(sql, val)
+    registros = cursor.fetchall()
+    con.close()
+
+    return make_response(jsonify(registros))
+
+@app.route("/clientes/eliminar", methods=["POST"])
+def eliminarCliente():
+    if not con.is_connected():
+        con.reconnect()
+
+    id = request.form["id"]
+
+    cursor = con.cursor(dictionary=True)
+    sql    = """
+    DELETE FROM clientes
+    WHERE idCliente = %s
     """
     val    = (id,)
 
